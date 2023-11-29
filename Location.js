9f9e663598eff2fcd23300acf849115be0de16af
Lory <147001347+LaurianeH-05@users.noreply.github.com>
2023-11-28 22:43:26 -0500

    Update Location.js

--- Location.js
+++ Location.js
@@ -2,22 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const leaveReviewBtn = document.getElementById("leaveReviewBtn");
     const reviewForm = document.getElementById("reviewForm");
     const closeReviewFormBtn = document.getElementById("closeReviewFormBtn");
-  
-    // Toggle review form visibility with smooth transition
+
     const toggleReviewForm = () => {
-      reviewForm.classList.toggle("hidden");
-      setTimeout(() => {
-        reviewForm.style.opacity = reviewForm.style.opacity === "0" ? "1" : "0";
-      }, 10);
+        reviewForm.classList.toggle("active");
     };
-  
+
     leaveReviewBtn.addEventListener("click", function () {
-      toggleReviewForm();
+        toggleReviewForm();
     });
-  
-    // Close the review form
+
     closeReviewFormBtn.addEventListener("click", function () {
-      toggleReviewForm();
+        toggleReviewForm();
     });
-  });
-  
+});
